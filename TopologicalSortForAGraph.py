4b27d4dfc76976cf9777706eaac95a082b8e363d
Divya Kattegummula <divyareddy0808@gmail.com>
2018-12-05 18:11:38 -0800

    Update TopologicalSortForAGraph.py

--- TopologicalSortForAGraph.py
+++ TopologicalSortForAGraph.py
@@ -13,30 +13,32 @@ def isCyclic(graph):
         return False
     
     return any(visit(v) for v in graph)
-    
+
+output_stack=[]
+
+def topology_sort(vertex):
+    if not visited_list[vertex]:
+        visited_list[vertex] = True
+        for neighbor in graph[vertex]:
+            topology_sort(neighbor)
+        output_stack.insert(0, vertex)
+  
     
 graph = {'A': set(['B']),
          'B': set(['D']),
          'C': set(['B']), 
          'D': set(['E']),
          'E': set([])}
+
 if isCyclic(graph):
   print('Graph is cyclic. Topological sort is not possible')
 else:
+  print('Given graph is a directed acyclic graph. Topological sort is possible ')
   visited_list = dict()
   for i in graph.keys():
       visited_list[i]=False
       
-  output_stack=[]
-
-  def topology_sort(vertex):
-      if not visited_list[vertex]:
-          visited_list[vertex] = True
-          for neighbor in graph[vertex]:
-              topology_sort(neighbor)
-          output_stack.insert(0, vertex)
-      
   for vertex in graph.keys():
       topology_sort(vertex)
-
+  print('Topological sort of the graph is:')
   print(output_stack)

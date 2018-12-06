d978fd97445cf4697c8801a2816d5f8a42c264b2
Divya Kattegummula <divyareddy0808@gmail.com>
2018-12-06 13:25:42 -0800

    Create treeTraversals.py

---
+++ treeTraversals.py
@@ -0,0 +1,50 @@
+class Node:
+  def __init__(self, value):
+    self.value=value
+    self.left=None
+    self.right=None
+
+
+#      1
+#    2     3
+#4     5
+
+
+#tree traversals
+# Inorder traversal -> L Root R
+# 4 2 5 1 3
+# Preorder traversal -> Root L R
+#1 2 4 5 3
+# Post order Traversal -> L R Root
+# 4 5 2 3 1
+
+def inorderTraversal(root):
+  if root is not None:
+    inorderTraversal(root.left)
+    print(root.value,end=' ')
+    inorderTraversal(root.right)
+
+def preorderTraversal(root):
+  if root is not None:
+    print(root.value,end=' ')
+    preorderTraversal(root.left)
+    preorderTraversal(root.right)
+
+def postorderTraversal(root):
+  if root is not None:
+    postorderTraversal(root.left)
+    postorderTraversal(root.right)
+    print(root.value,end=' ')
+
+
+root= Node(1)
+root.left = Node(2)
+root.right=Node(3)
+root.left.left=Node(4)
+root.left.right=Node(5)
+
+inorderTraversal(root)
+print()
+preorderTraversal(root)
+print()
+postorderTraversal(root)

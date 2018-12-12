ecc243125caf5506cff0856ce9defc9cf5524b52
Divya Kattegummula <divyareddy0808@gmail.com>
2018-12-11 16:06:54 -0800

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -7,7 +7,7 @@ def BFS(vertex):
             node=queue.pop(0)
             output_queue.append(node)
             for neighbor in graph[node]:
-                BFS(vertex)
+                BFS(neighbor)
         
         
 graph = { 1 : set([2,5]), 2: set([3,4]), 3:set([]),4:set([]), 5:set([6,7]) , 6: set([]), 7 :set([]) }
